9d95c309bcfbcbe24f03fd7201213553fa193e96
hao quang <haoquangms@gmail.com>
2020-11-23 15:47:20 +0000

    mulprocessing

--- src/api.py
+++ src/api.py
@@ -89,8 +89,17 @@ def warmup():
 
     with graph.as_default():
         v1 = network_eval.predict(specs)
+def calcSpec(q,filename):
+    specs = ut.load_data(filename, win_length=params['win_length'], sr=params['sampling_rate'],
+                                hop_length=params['hop_length'], n_fft=params['nfft'],
+                                spec_len=params['spec_len'], mode='eval')
+    q.put(np.expand_dims(np.expand_dims(specs, 0), -1))
 # Flask
+
 app = Flask(__name__)
+from multiprocessing import Process, Queue
+import timeit
+
 
 @app.route("/api/predict", methods=['POST'])
 def api_predict():
@@ -98,13 +107,10 @@ def api_predict():
     Required params:
         audio_1, audio_2
     """
-    import timeit
-
-    
     audio_file_1 = request.files['audio_1'] # Required
     audio_file_2 = request.files['audio_2'] # Required
     counttime = 0
-    start = timeit.default_timer()
+    point1 = timeit.default_timer()
     if audio_file_1:
         filename_1 = os.path.join(AUDIO_STORAGE,secure_filename(audio_file_1.filename))
         audio_file_1.save(filename_1) # Save audio in audio_storage, path: audio_storage/filename_1
@@ -112,47 +118,40 @@ def api_predict():
     if audio_file_2:
         filename_2 = os.path.join(AUDIO_STORAGE,secure_filename(audio_file_2.filename))
         audio_file_2.save(filename_2) # Save audio in audio_storage, path: audio_storage/filename_2
-    stop = timeit.default_timer()
-    counttime += stop - start
-    print('Time save file: ', stop - start)
     v1 = None
     v2 = None
-    # Import code here
-    start = timeit.default_timer()
-    ch1 = calcHash(filename_1) 
-    ch2 = calcHash(filename_2) 
-    stop = timeit.default_timer()
-    counttime += stop - start
-    print('Time hash: ', stop - start)
-    start = timeit.default_timer()
-    if( ch1 in cacheTable.keys()):
-        v1 = cacheTable[ch1]
-    else:
-        specs = ut.load_data(filename_1, win_length=params['win_length'], sr=params['sampling_rate'],
-                                hop_length=params['hop_length'], n_fft=params['nfft'],
-                                spec_len=params['spec_len'], mode='eval')
-        specs = np.expand_dims(np.expand_dims(specs, 0), -1)
-
-        with graph.as_default():
-            v1 = network_eval.predict(specs)
-        cacheTable[ch1] = v1
+    point2 = timeit.default_timer()
     
-    if( ch2 in cacheTable.keys()):
-        v2 = cacheTable[ch2]
-    else:
-        specs = ut.load_data(filename_2, win_length=params['win_length'], sr=params['sampling_rate'],
-                                hop_length=params['hop_length'], n_fft=params['nfft'],
-                                spec_len=params['spec_len'], mode='eval')
-        specs = np.expand_dims(np.expand_dims(specs, 0), -1)
-        with graph.as_default():
-            v2 = network_eval.predict(specs)
-        cacheTable[ch2] = v2
+    Q = Queue()
+    def calcSpec(filename):
+        specs = ut.load_data(filename, win_length=params['win_length'], sr=params['sampling_rate'],
+                                    hop_length=params['hop_length'], n_fft=params['nfft'],
+                                    spec_len=params['spec_len'], mode='eval')
+        Q.put(np.expand_dims(np.expand_dims(specs, 0), -1))
 
+    p1 = Process(target=calcSpec, args=(filename_1,))
+    p2 = Process(target=calcSpec, args=(filename_2,))
+
+    p1.start()
+    p2.start()
+    spec1 = Q.get()
+    spec2 = Q.get()
+    p1.join()
+    p2.join()
+    point3 = timeit.default_timer()
+    
+    print('Time calc specs: ', point3 - point2)
+
+    with graph.as_default():
+        v1 = network_eval.predict(spec1)
+    
+    with graph.as_default():
+        v2 = network_eval.predict(spec2)
     label = (np.linalg.norm(v1 - v2) < 0.547742) * 1
-    stop = timeit.default_timer()
-    counttime += stop - start
-    print('Time: ', stop - start)
-    print("Time overal ", counttime)
+
+    point4 = timeit.default_timer()
+
+    print("Time overal ", point4- point1)
     return jsonify(
         label = label
     )
@@ -160,4 +159,6 @@ def api_predict():
 if __name__ == '__main__':
     load_model()
     warmup()
+    
     app.run(host='0.0.0.0', port='6677', debug=False)
+    
